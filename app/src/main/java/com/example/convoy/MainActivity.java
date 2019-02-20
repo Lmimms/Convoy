f440af0090281623ff5beb326bcb97abbc4c5a78
Lgmimms <lgmimms@crimson.ua.edu>
2019-02-20 13:20:53 -0600

    added password checking

--- app/src/main/java/com/example/convoy/MainActivity.java
+++ app/src/main/java/com/example/convoy/MainActivity.java
@@ -24,12 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         drawer = findViewById(R.id.drawer_layout);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        //Intent mapIntent = new Intent(this, LoginActivity.class);
-        //startActivity(mapIntent);
+      //  ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+        //        R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+       // drawer.addDrawerListener(toggle);
+        //toggle.syncState();
+        Intent mapIntent = new Intent(this, LoginActivity.class);
+        startActivity(mapIntent);
     }
 
     @Override
