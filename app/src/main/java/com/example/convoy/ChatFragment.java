f3af8f116750710f4096ad4f774114961230534f
Lgmimms <lgmimms@crimson.ua.edu>
2019-02-26 14:20:58 -0600

    changed main activity to login in the gradle

--- app/src/main/java/com/example/convoy/ChatFragment.java
+++ app/src/main/java/com/example/convoy/ChatFragment.java
@@ -8,11 +8,65 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Date;
+
 public class ChatFragment extends Fragment {
+    // Firebase instance variables
+    private DatabaseReference mFirebaseDatabaseReference;
+
+    public class ChatMessage {
+
+        private String messageText;
+        private String messageUser;
+        private long messageTime;
+
+        public ChatMessage(String messageText, String messageUser) {
+            this.messageText = messageText;
+            this.messageUser = messageUser;
+
+            // Initialize to current time
+            messageTime = new Date().getTime();
+        }
+
+        public ChatMessage(){
+
+        }
+
+        public String getMessageText() {
+            return messageText;
+        }
+
+        public void setMessageText(String messageText) {
+            this.messageText = messageText;
+        }
+
+        public String getMessageUser() {
+            return messageUser;
+        }
+
+        public void setMessageUser(String messageUser) {
+            this.messageUser = messageUser;
+        }
+
+        public long getMessageTime() {
+            return messageTime;
+        }
+
+        public void setMessageTime(long messageTime) {
+            this.messageTime = messageTime;
+        }
+    }
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chat, container, false);
+
+
+
     }
 }
